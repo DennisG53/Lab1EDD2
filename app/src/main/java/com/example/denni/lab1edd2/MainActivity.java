4f49c131ddf50ac7e3e37dbaa337fef896991dda
Laboratorio de Software T-213 <t213@laburl>
2018-09-13 08:29:05 -0600

    pureba

--- app/src/main/java/com/example/denni/lab1edd2/MainActivity.java
+++ app/src/main/java/com/example/denni/lab1edd2/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.denni.lab1edd2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView Lista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void CargarDatos(View view){
+        List<String> Listado = new ArrayList<String>();
+
+        InputStream is = this.getResources().openRawResource(R.raw.data);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        if ( is != null){
+            while ( (linea=reader.readLine()!=nul) ){
+
+            }
+        }
+    }
+
+
+
 }
